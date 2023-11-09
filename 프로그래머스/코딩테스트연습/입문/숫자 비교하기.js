94a4344685aa1d05e02a44e25d0a02e5441580bb
pompomko <sdw12334@gmail.com>
2023-11-09 10:29:04 +0900

    숫자 비교하기

---
+++ 프로그래머스/코딩테스트연습/입문/숫자 비교하기.js
@@ -0,0 +1,14 @@
+const num1 = 11;
+const num2 = 11;
+
+function solution(num1, num2) {
+  var answer = 0;
+
+  //num1-num2 가 0이나오면 1출력
+  //num1 -num2 가 0이외면 -1출력
+  if (num1 == num2) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
